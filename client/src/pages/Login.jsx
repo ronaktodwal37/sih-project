cfaaea31b08ca5c3c7ffcef8490132989f545caa
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-22 22:34:51 +0530

    Login dashboard

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 function Login() {
     const { login } = useContext(AuthContext);
     const navigate = useNavigate();
-    const [email, setEmail] = useState('');
+    // const [email, setEmail] = useState('');
     // const [password, setPassword] = useState('');
     // const [errorMsg, setErrorMsg] = useState('');
 
