46bc027d1e56431509d9b75fbeed76283bd78415
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-22 22:36:20 +0530

    Login dashboard

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -7,7 +7,7 @@ function Login() {
     // const navigate = useNavigate();
     // const [email, setEmail] = useState('');
     // const [password, setPassword] = useState('');
-    // const [errorMsg, setErrorMsg] = useState('');
+    const [errorMsg, setErrorMsg] = useState('');
 
     const onSubmit = async (e) => {
         e.preventDefault();
