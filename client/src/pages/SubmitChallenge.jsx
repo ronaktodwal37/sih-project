1d32fe10d8011c288691c35c1db90ae6f863f544
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-22 22:38:56 +0530

    Submit Challange

--- client/src/pages/SubmitChallenge.jsx
+++ client/src/pages/SubmitChallenge.jsx
@@ -7,7 +7,7 @@ import { AuthContext } from '../context/AuthContext';
 function SubmitChallenge() {
     const { user } = useContext(AuthContext);
     const navigate = useNavigate();
-    // const [step, setStep] = useState(1);
+    const [step, setStep] = useState(1);
     // const { register, handleSubmit, formState: { errors } } = useForm();
 
     // const nextStep = (e) => {
