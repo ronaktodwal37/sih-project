238819109fc662e43a017a9a134cdeb288ddf7ee
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-22 22:26:17 +0530

    govt dashboard

--- client/src/pages/GovernmentDashboard.jsx
+++ client/src/pages/GovernmentDashboard.jsx
@@ -71,7 +71,7 @@ function GovernmentDashboard() {
                                 // innerRadius={60}
                                 // outerRadius={80}
                                 // fill="#8884d8"
-                                paddingAngle={5}
+                                // paddingAngle={5}
                                 dataKey="value"
                                 label
                             >
