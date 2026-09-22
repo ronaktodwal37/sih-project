3492387bf3cfba10a93d65266cf6092e66f6a253
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-22 22:25:11 +0530

    govt dashboard

--- client/src/pages/GovernmentDashboard.jsx
+++ client/src/pages/GovernmentDashboard.jsx
@@ -13,7 +13,7 @@ function GovernmentDashboard() {
     const loadAnalytics = async () => {
         try {
             // const res = await api.get('/analytics/government');
-            setData(res.data);
+            // setData(res.data);
         } catch (err) {
             console.error(err);
         }
