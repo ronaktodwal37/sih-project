a6b2c5de35cc1ccbc6887be41563066210ab9754
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-22 22:27:57 +0530

    govt dashboard

--- client/src/pages/GovernmentDashboard.jsx
+++ client/src/pages/GovernmentDashboard.jsx
@@ -68,8 +68,8 @@ function GovernmentDashboard() {
                                 data={data.charts.districts}
                                 cx="50%"
                                 cy="50%"
-                                // innerRadius={60}
-                                // outerRadius={80}
+                                innerRadius={60}
+                                outerRadius={80}
                                 // fill="#8884d8"
                                 // paddingAngle={5}
                                 // dataKey="value"
