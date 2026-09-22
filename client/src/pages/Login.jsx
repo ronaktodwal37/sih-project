4e16bf60f23c4595166c770ee1710b341e5e004c
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-22 22:35:38 +0530

    Login dashboard

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -17,7 +17,7 @@ function Login() {
 
             if (u.role === 'Citizen') navigate('/citizen/challenges/new');
             // else if (u.role === 'University') navigate('/university/dashboard');
-            else if (u.role === 'Government' || u.role === 'Admin') navigate('/government/dashboard');
+            // else if (u.role === 'Government' || u.role === 'Admin') navigate('/government/dashboard');
             else navigate('/');
 
         } catch (err) {
