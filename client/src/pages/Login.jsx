0efd74f825a54dddd726d15b57fe7c94c1ae12be
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-22 22:35:54 +0530

    Login dashboard

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -15,10 +15,10 @@ function Login() {
         try {
             const u = await login({ email, password });
 
-            // if (u.role === 'Citizen') navigate('/citizen/challenges/new');
-            // else if (u.role === 'University') navigate('/university/dashboard');
-            // else if (u.role === 'Government' || u.role === 'Admin') navigate('/government/dashboard');
-            // else navigate('/');
+            if (u.role === 'Citizen') navigate('/citizen/challenges/new');
+            else if (u.role === 'University') navigate('/university/dashboard');
+            else if (u.role === 'Government' || u.role === 'Admin') navigate('/government/dashboard');
+            else navigate('/');
 
         } catch (err) {
             setErrorMsg(err.response?.data?.message || 'Login failed. Invalid credentials.');
